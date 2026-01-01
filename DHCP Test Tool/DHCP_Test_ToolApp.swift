b0bb7a16e344b8d176f0074d2d220c3a663bc4b9
George Babichev <gbabichev1@gmail.com>
2025-12-31 22:53:47 -0500

    Initial Commit

---
+++ DHCP Test Tool/DHCP_Test_ToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  DHCP_Test_ToolApp.swift
+//  DHCP Test Tool
+//
+//  Created by George Babichev on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct DHCP_Test_ToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
